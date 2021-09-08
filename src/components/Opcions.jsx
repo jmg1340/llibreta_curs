58ad4bb63a324b228dfcac924c739c759bf2e12c
jordi <jordi.miserachs@gmail.com>
2021-09-08 17:02:05 +0200

    commit 3

--- src/components/Opcions.jsx
+++ src/components/Opcions.jsx
@@ -1,87 +1,183 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core';
+import React, { Component, useState } from 'react';
+import { Button, makeStyles, withStyles } from '@material-ui/core';
 
 import OpcionsRang from "./OpcionsRang.jsx"
 import OpcionsFestius from "./OpcionsFestius.jsx"
 import OpcionsHoraris from "./OpcionsHoraris.jsx"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
+// const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
+	root: {
     '& > *': {
       margin: theme.spacing(1),
       width: '50ch'
     },
   },
-}));
-
-export default function Opcions() {
-  const classes = useStyles();
-
-	const [dataInici, setDataInici] = useState('')
-	const [dataFinal, setDataFinal] = useState('')
-	const [festius, setFestius] = useState([
-		"11/9", "12/10", "11/1", "6/12", "8/12", "1/5", "24/6"
-	])
-	const [horaris, setHoraris] = useState({
-		Dll: [
-			{hora1: 'h1', materia1: 'DllM1'},
-			{hora2: 'h2', materia2: 'DllM2'},
-			{hora3: 'h3', materia3: 'DllM3'},
-			{hora4: 'h4', materia4: 'DllM4'},
-			{hora5: 'h5', materia5: 'DllM5'},
-			{hora6: 'h6', materia6: 'DllM6'},
-			{hora7: 'h7', materia7: 'DllM7'},
-			{hora8: 'h8', materia8: 'DllM8'},
-		],
-		Dm:  [
-			{hora1: 'h1', materia1: 'DmM1'},
-			{hora2: 'h2', materia2: 'DmM2'},
-			{hora3: 'h3', materia3: 'DmM3'},
-			{hora4: 'h4', materia4: 'DmM4'},
-			{hora5: 'h5', materia5: 'DmM5'},
-			{hora6: 'h6', materia6: 'DmM6'},
-			{hora7: 'h7', materia7: 'DmM7'},
-			{hora8: 'h8', materia8: 'DmM8'},
-		],
-		Dcr: [
-			{hora1: 'h1', materia1: 'DcrM1'},
-			{hora2: 'h2', materia2: 'DcrM2'},
-			{hora3: 'h3', materia3: 'DcrM3'},
-			{hora4: 'h4', materia4: 'DcrM4'},
-			{hora5: 'h5', materia5: 'DcrM5'},
-			{hora6: 'h6', materia6: 'DcrM6'},
-			{hora7: 'h7', materia7: 'DcrM7'},
-			{hora8: 'h8', materia8: 'DcrM8'},
-		],
-		Dj:  [
-			{hora1: 'h1', materia1: 'DjM1'},
-			{hora2: 'h2', materia2: 'DjM2'},
-			{hora3: 'h3', materia3: 'DjM3'},
-			{hora4: 'h4', materia4: 'DjM4'},
-			{hora5: 'h5', materia5: 'DjM5'},
-			{hora6: 'h6', materia6: 'DjM6'},
-			{hora7: 'h7', materia7: 'DjM7'},
-			{hora8: 'h8', materia8: 'DjM8'},
-		],
-		Dv:  [
-			{hora1: 'h1', materia1: 'DvM1'},
-			{hora2: 'h2', materia2: 'DvM2'},
-			{hora3: 'h3', materia3: 'DvM3'},
-			{hora4: 'h4', materia4: 'DvM4'},
-			{hora5: 'h5', materia5: 'DvM5'},
-			{hora6: 'h6', materia6: 'DvM6'},
-			{hora7: 'h7', materia7: 'DvM7'},
-			{hora8: 'h8', materia8: 'DvM8'},
-		]
-	})
+});
 
 
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <OpcionsRang dataInici={ dataInici } dataFinal={ dataFinal } />
-      <OpcionsFestius festius={ festius } />
-      <OpcionsHoraris horaris={ horaris } />
 
-    </form>
-  );
+class Opcions extends Component {
+  // const classes = useStyles();
+
+
+	state = {
+		dataInici : '',
+		dataFinal : '',
+		festius: [ "11/9", "12/10", "11/1", "6/12", "8/12", "1/5", "24/6"	],
+		horaris: {
+			Dll: [
+				{hora1: 'h1', materia1: 'DllM1'},
+				{hora2: 'h2', materia2: 'DllM2'},
+				{hora3: 'h3', materia3: 'DllM3'},
+				{hora4: 'h4', materia4: 'DllM4'},
+				{hora5: 'h5', materia5: 'DllM5'},
+				{hora6: 'h6', materia6: 'DllM6'},
+				{hora7: 'h7', materia7: 'DllM7'},
+				{hora8: 'h8', materia8: 'DllM8'},
+			],
+			Dm:  [
+				{hora1: 'h1', materia1: 'DmM1'},
+				{hora2: 'h2', materia2: 'DmM2'},
+				{hora3: 'h3', materia3: 'DmM3'},
+				{hora4: 'h4', materia4: 'DmM4'},
+				{hora5: 'h5', materia5: 'DmM5'},
+				{hora6: 'h6', materia6: 'DmM6'},
+				{hora7: 'h7', materia7: 'DmM7'},
+				{hora8: 'h8', materia8: 'DmM8'},
+			],
+			Dcr: [
+				{hora1: 'h1', materia1: 'DcrM1'},
+				{hora2: 'h2', materia2: 'DcrM2'},
+				{hora3: 'h3', materia3: 'DcrM3'},
+				{hora4: 'h4', materia4: 'DcrM4'},
+				{hora5: 'h5', materia5: 'DcrM5'},
+				{hora6: 'h6', materia6: 'DcrM6'},
+				{hora7: 'h7', materia7: 'DcrM7'},
+				{hora8: 'h8', materia8: 'DcrM8'},
+			],
+			Dj:  [
+				{hora1: 'h1', materia1: 'DjM1'},
+				{hora2: 'h2', materia2: 'DjM2'},
+				{hora3: 'h3', materia3: 'DjM3'},
+				{hora4: 'h4', materia4: 'DjM4'},
+				{hora5: 'h5', materia5: 'DjM5'},
+				{hora6: 'h6', materia6: 'DjM6'},
+				{hora7: 'h7', materia7: 'DjM7'},
+				{hora8: 'h8', materia8: 'DjM8'},
+			],
+			Dv:  [
+				{hora1: 'h1', materia1: 'DvM1'},
+				{hora2: 'h2', materia2: 'DvM2'},
+				{hora3: 'h3', materia3: 'DvM3'},
+				{hora4: 'h4', materia4: 'DvM4'},
+				{hora5: 'h5', materia5: 'DvM5'},
+				{hora6: 'h6', materia6: 'DvM6'},
+				{hora7: 'h7', materia7: 'DvM7'},
+				{hora8: 'h8', materia8: 'DvM8'},
+			]
+		}
+	}
+
+
+
+	// const [dataInici, setDataInici] = useState('')
+	// const [dataFinal, setDataFinal] = useState('')
+
+	// const [festius, setFestius] = useState([
+	// 	"11/9", "12/10", "11/1", "6/12", "8/12", "1/5", "24/6"
+	// ])
+
+	// const [horaris, setHoraris] = useState({
+	// 	Dll: [
+	// 		{hora1: 'h1', materia1: 'DllM1'},
+	// 		{hora2: 'h2', materia2: 'DllM2'},
+	// 		{hora3: 'h3', materia3: 'DllM3'},
+	// 		{hora4: 'h4', materia4: 'DllM4'},
+	// 		{hora5: 'h5', materia5: 'DllM5'},
+	// 		{hora6: 'h6', materia6: 'DllM6'},
+	// 		{hora7: 'h7', materia7: 'DllM7'},
+	// 		{hora8: 'h8', materia8: 'DllM8'},
+	// 	],
+	// 	Dm:  [
+	// 		{hora1: 'h1', materia1: 'DmM1'},
+	// 		{hora2: 'h2', materia2: 'DmM2'},
+	// 		{hora3: 'h3', materia3: 'DmM3'},
+	// 		{hora4: 'h4', materia4: 'DmM4'},
+	// 		{hora5: 'h5', materia5: 'DmM5'},
+	// 		{hora6: 'h6', materia6: 'DmM6'},
+	// 		{hora7: 'h7', materia7: 'DmM7'},
+	// 		{hora8: 'h8', materia8: 'DmM8'},
+	// 	],
+	// 	Dcr: [
+	// 		{hora1: 'h1', materia1: 'DcrM1'},
+	// 		{hora2: 'h2', materia2: 'DcrM2'},
+	// 		{hora3: 'h3', materia3: 'DcrM3'},
+	// 		{hora4: 'h4', materia4: 'DcrM4'},
+	// 		{hora5: 'h5', materia5: 'DcrM5'},
+	// 		{hora6: 'h6', materia6: 'DcrM6'},
+	// 		{hora7: 'h7', materia7: 'DcrM7'},
+	// 		{hora8: 'h8', materia8: 'DcrM8'},
+	// 	],
+	// 	Dj:  [
+	// 		{hora1: 'h1', materia1: 'DjM1'},
+	// 		{hora2: 'h2', materia2: 'DjM2'},
+	// 		{hora3: 'h3', materia3: 'DjM3'},
+	// 		{hora4: 'h4', materia4: 'DjM4'},
+	// 		{hora5: 'h5', materia5: 'DjM5'},
+	// 		{hora6: 'h6', materia6: 'DjM6'},
+	// 		{hora7: 'h7', materia7: 'DjM7'},
+	// 		{hora8: 'h8', materia8: 'DjM8'},
+	// 	],
+	// 	Dv:  [
+	// 		{hora1: 'h1', materia1: 'DvM1'},
+	// 		{hora2: 'h2', materia2: 'DvM2'},
+	// 		{hora3: 'h3', materia3: 'DvM3'},
+	// 		{hora4: 'h4', materia4: 'DvM4'},
+	// 		{hora5: 'h5', materia5: 'DvM5'},
+	// 		{hora6: 'h6', materia6: 'DvM6'},
+	// 		{hora7: 'h7', materia7: 'DvM7'},
+	// 		{hora8: 'h8', materia8: 'DvM8'},
+	// 	]
+	// })
+
+
+	guardarDates = (vDataInici, vDataFinal) => {
+		console.log("modificant data inici...")
+		console.log(`vDataInici: ${vDataInici}`, `vDataFinal: ${vDataFinal}`)
+		this.setState({dataInici: vDataInici})
+		this.setState({dataFinal: vDataFinal})
+	}
+
+	OpcsRng = React.createRef()
+	generarPDF = (event) => {
+		event.preventDefault()
+
+		console.log("No s'ha reiniciat")
+		console.log(this.OpcsRng.current)
+		this.OpcsRng.current.guardar()
+		
+	}
+
+
+	render () {
+		const { classes } = this.props;
+
+		return (
+			
+			<form className={classes.root} noValidate autoComplete="off" onSubmit={this.generarPDF}>
+				{/* <OpcionsRang dataInici={ dataInici } dataFinal={ dataFinal } guardarDates={ guardarDates }/> */}
+				<OpcionsRang ref={this.OpcsRng}   guardarDates={ this.guardarDates }/>
+				{/* <OpcionsFestius festius={ festius } /> */}
+				{/* <OpcionsHoraris horaris={ horaris } /> */}
+
+				<Button type="submit" color="primary" variant="contained" onSubmit={this.generarPDF}>Generar...</Button>
+			</form>
+
+
+		);		
+	}
+
 }
+
+export default withStyles(styles)(Opcions)
--- src/components/OpcionsRang.jsx
+++ src/components/OpcionsRang.jsx
@@ -1,63 +1,116 @@
-import React from "react";
-import { Grid, TextField, makeStyles } from "@material-ui/core"
+import React, { Component, useState } from "react";
+import { Grid, TextField, makeStyles, withStyles } from "@material-ui/core"
 
 
-const useStyles = makeStyles((theme) => ({
-  clOpcionsR: {
-    '& > *': {
-      margin: theme.spacing(2),
-      // width: '25ch',
-    },
+// const useStyles = makeStyles((theme) => ({
+// 	clOpcionsR: {
+// 		'& > *': {
+// 			margin: theme.spacing(2),
+// 			// width: '25ch',
+// 		},
+// 		border: '1px solid black',
+// 		marginBottom: '5ch'
+// 	},
+	
+// }));
+	
+
+const styles = (theme) => ({
+	clOpcionsR: {
+		'& > *': {
+			margin: theme.spacing(2),
+			// width: '25ch',
+		},
 		border: '1px solid black',
 		marginBottom: '5ch'
-  },
+	},
 	
-}));
-
-
-
-export default function OpcionsRang (props) {
-  const classes = useStyles();
+});
 	
+
+
+
+
+
+
+class OpcionsRang extends Component  {
+  // classes = useStyles();
+
+
+	// [dataInici2, setDataInici2] = useState('')
+	// [dataFinal2, setDataFinal2] = useState('')
+	
+	
+	state = {
+		dataInici2: '',
+		dataFinal2: ''
+	}
+
+	methodToOnChange = (event) => {
+		console.log( { [event.target.name] : event.target.value } )
+		// switch (event.target.name) {
+		// 	case "dataInici2": setDataInici2(event.target.value); break;
+		// 	case "dataFinal2": setDataFinal2( event.target.value); break
+		// }
+		new Promise ((resolve, reject) => {
+			this.setState({ [event.target.name] : event.target.value })
+			resolve()
+		}).then ( () => {
+			console.log(`STATE: dataInici2 : ${this.state.dataInici2}`, `STATE: dataFinal2 : ${this.state.dataFinal2}`)
+			// this.props.guardarDates( this.state.dataInici2, this.state.dataFinal2 )
+		})
+		
+	}
+
+	guardar = () => { this.props.guardarDates( this.state.dataInici2, this.state.dataFinal2 ) }
+
+	// console.log(props)
+	// console.log(props.guardarDates())
+	render() {
+		const { classes } = this.props;
 		return (
-      <Grid
-        container
-        spacing={2}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        wrap="nowrap"
+			<Grid
+				container
+				spacing={2}
+				direction="row"
+				justifyContent="center"
+				alignItems="center"
+				wrap="nowrap"
 				className={ classes.clOpcionsR }
-        
-      >
-        
-      
-          <TextField 
-            id="standard-basic" 
-            label="Data inicial curs" 
-            variant="outlined"
+				
+			>
+				
+			
+					<TextField 
+						name="dataInici2" 
+						label="Data inicial curs" 
+						variant="outlined"
 						size="small" 
-            InputLabelProps={{
-                shrink: true,
-            }}
-						value={props.dataInici}
+						InputLabelProps={{
+								shrink: true,
+						}}
+						value={this.dataInici2}
+						onChange={this.methodToOnChange}
 
-          />
-          <TextField 
-            id="standard-basic" 
-            label="Data final curs" 
-            variant="outlined"
+					/>
+					<TextField 
+						name="dataFinal2" 
+						label="Data final curs" 
+						variant="outlined"
 						size="small" 
-            InputLabelProps={{
-                shrink: true,
-            }}
-						value={props.dataFinal}
-
-          />   
-      
-      
-      
-      </Grid>			
+						InputLabelProps={{
+								shrink: true,
+						}}
+						value={this.dataFinal2}
+						onChange={this.methodToOnChange}
+					/>   
+			
+			
+			
+			</Grid>			
 		)
-  
+	}
 }
+
+
+export default withStyles(styles)(OpcionsRang)
